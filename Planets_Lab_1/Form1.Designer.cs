cbf83f23fc6bf0e0d635c64cc8a5175b03423c0f
Doubles Double <extremecookie.ec@gmail.com>
2021-02-28 15:49:38 +0300

    Добавьте файлы проекта.

---
+++ Planets_Lab_1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace Planets_Lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sceneControl1 = new SharpGL.SceneControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.sceneControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sceneControl1
+            // 
+            this.sceneControl1.DrawFPS = true;
+            this.sceneControl1.FrameRate = 60;
+            this.sceneControl1.Location = new System.Drawing.Point(0, 0);
+            this.sceneControl1.Name = "sceneControl1";
+            this.sceneControl1.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.sceneControl1.RenderContextType = SharpGL.RenderContextType.FBO;
+            this.sceneControl1.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.sceneControl1.Size = new System.Drawing.Size(1004, 598);
+            this.sceneControl1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 604);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 638);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sceneControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sceneControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SharpGL.SceneControl sceneControl1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Planets_Lab_1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using SharpGL;
+using SharpGL.SceneGraph.Assets;
+using SharpGL.SceneGraph.Cameras;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Planets_Lab_1
+{
+    public partial class Form1 : Form
+    {
+        SharpGL.SceneGraph.Quadrics.Sphere Earth = new SharpGL.SceneGraph.Quadrics.Sphere();
+        SharpGL.SceneGraph.Quadrics.Sphere Moon = new SharpGL.SceneGraph.Quadrics.Sphere();
+        SharpGL.SceneGraph.Quadrics.Sphere Sun = new SharpGL.SceneGraph.Quadrics.Sphere();
+        Texture texture = new Texture();
+     
+        public Form1()
+        {
+            InitializeComponent();
+
+            SharpGL.SceneGraph.Assets.Material SunMaterial = new SharpGL.SceneGraph.Assets.Material();
+            SharpGL.SceneGraph.Assets.Material EarthMaterial = new SharpGL.SceneGraph.Assets.Material();
+            SharpGL.SceneGraph.Assets.Material MoonMaterial = new SharpGL.SceneGraph.Assets.Material();
+            //SharpGL.SceneGraph.Assets.Texture texture = new SharpGL.SceneGraph.Assets.Texture();
+            //SharpGL.SceneGraph.Assets.Texture texture1 = new SharpGL.SceneGraph.Assets.Texture();
+
+            // material1.Ambient = Color.FromArgb(100, Color.Red);
+
+
+            // OpenGLControl openglCtr = new SharpGL.OpenGLControl();
+            // texture.Create(openglCtr.OpenGL, "Sun.bmp");
+
+            //// texture1.Create(openglCtr.OpenGL, "Crate.bmp");
+
+            //// material.Texture = texture1;
+            // material1.Texture = texture;
+
+
+            //SharpGL.OpenGL gl = this.sceneControl1.OpenGL;
+            //texture.Create(gl, "Crate.bmp");
+            //texture.Bind(gl);
+
+            //uint[] textureID = new uint[1];
+            //byte[] colors = new byte[256 * 256 * 4];
+            //gl.BindTexture(OpenGL.GL_TEXTURE_2D, textureID[0]);
+            //gl.TexImage2D(OpenGL.GL_TEXTURE_2D, 0, (int)OpenGL.GL_TEXTURE, 256, 256, 50, OpenGL.GL_TEXTURE, OpenGL.GL_BYTE, colors);
+
+            SunMaterial.Diffuse = Color.Orange;
+            EarthMaterial.Diffuse = Color.Cyan;
+            MoonMaterial.Diffuse = Color.Silver;
+            SunMaterial.Ambient = Color.FromArgb(0, Color.Orange);
+            EarthMaterial.Ambient = Color.FromArgb(255, Color.Blue);
+            MoonMaterial.Ambient = Color.FromArgb(255, Color.Silver);
+
+            SunMaterial.Texture = texture;
+
+            Sun.Material = SunMaterial;
+            Moon.Material = MoonMaterial;
+            Earth.Material = EarthMaterial;
+
+            Earth.Radius = 2;
+            Moon.Radius = 0.5;
+            Sun.Radius = 4;
+
+            sceneControl1.Scene.SceneContainer.AddChild(Earth);
+            sceneControl1.Scene.SceneContainer.AddChild(Moon);
+            sceneControl1.Scene.SceneContainer.AddChild(Sun);
+
+
+            sceneControl1.Scene.RenderBoundingVolumes = !sceneControl1.Scene.RenderBoundingVolumes;
+
+
+ 
+            //  Create the arcball camera.
+
+        }
+        double rotate = 0;
+        double i = 0;
+        double j = 0;
+        private void Tick(object sender, EventArgs e)
+        {
+            Earth.Transformation.RotateZ = (float)j;
+            Moon.Transformation.RotateZ = (float)rotate;
+
+            Earth.Transformation.TranslateX = (float)(10*Math.Cos(i));
+            Earth.Transformation.TranslateY = (float)(10*Math.Sin(i));
+
+
+            Moon.Transformation.TranslateX = (float)(10 * Math.Cos(i)) + (float)(3 * Math.Cos(j));
+            Moon.Transformation.TranslateY = (float)(10 * Math.Sin(i)) + (float)(3 * Math.Sin(j));
+
+            j += 0.05;
+            i+= 0.001;
+            rotate -= 0.05;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //  Destroy the existing texture.
+                texture.Destroy(sceneControl1.OpenGL);
+
+                //  Create a new texture.
+                texture.Create(sceneControl1.OpenGL, openFileDialog1.FileName);
+
+                //  Redraw.
+                sceneControl1.Invalidate();
+            }
+        }
+    }
+}
